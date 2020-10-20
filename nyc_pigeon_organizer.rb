ddecd8548b5d9f17dc4c248166ac81ab276de4d1
Sam Gold <sgold5757@gmail.com>
2020-10-20 22:36:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, specifics|
+    specifics.each do |specific, name_array|
+      name_array.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][attribute] == nil
+          pigeon_list[name][attribute] = []
+        end
+        if !pigeon_list[name][attribute].include?(specific.to_s)
+          pigeon_list[name][attribute] << specific.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
